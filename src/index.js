a939c57dc0fc2410140a6c93bdb0b82e825e32d4
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-02-04 12:07:40 +0100

    event.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // Challenge 1
 // Add click eventListener to the button. Alert `It is 18 degrees`
-function click(event) {
+
+// Challenge 2
+// When the button is clicked, replace button label by ‘18 degrees’
+
+function click() {
+  let button = document.querySelector("button");
+  button.innerHTML = "18 degrees";
   alert("It is 18 degrees");
 }
 let clickButton = document.querySelector("button");
 clickButton.addEventListener("click", click);
-
-// Challenge 2
-// When the button is clicked, replace button label by ‘18 degrees’
-clickButton.innerHTML = "18 degrees";
